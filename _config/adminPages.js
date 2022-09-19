30f095fe93dbd715ad1989afb6354f7872398f97
Wilmer Cristancho <wilmerc@imaginacolombia.com>
2022-09-19 10:32:31 -0500

    add crud

--- _config/adminPages.js
+++ _config/adminPages.js
@@ -1,2 +1,17 @@
 export default {
+    cargo: {
+        //permission: 'page.pages.manage',
+        activated: true,
+        authenticated: true,
+        path: '/cargo/index',
+        name: 'qcargoagione.admin.cargo',
+        crud : import('../_crud/cargo.vue'),
+        page: () => import('@imagina/qcrud/_pages/admin/crudPage'),
+        layout: () => import('@imagina/qsite/_layouts/master.vue'),
+        title: 'ireports.cms.sidebar.reportsInfo',
+        icon: 'fas fa-file-alt',
+        subHeader: {
+          refresh: true,
+        }
+    }
 }
