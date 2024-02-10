056672e4c75cd68510c4ab83cf7ec777e97525c0
Noith <nmontealegre@imaginacolombia.com>
2024-02-10 11:42:13 -0500

    Fixed: change import from @imagina/qsite/_layouts to layouts

--- _config/adminPages.js
+++ _config/adminPages.js
@@ -6,10 +6,10 @@ export default {
         path: '/awb-tracking/index',
         name: 'qcargoagione.admin.cargoAdmin',
         crud : import('../_crud/cargo.vue'),
-        page: () => import('@imagina/qcrud/_pages/admin/crudPage'),
-        layout: () => import('@imagina/qsite/_layouts/master.vue'),
+        page: () => import('modules/qcrud/_pages/admin/crudPage'),
+        layout: () => import('layouts/master.vue'),
         title: 'icargo.cms.sidebar.awbTracking',
-        icon: 'fa-light fa-plane-departure', 
+        icon: 'fa-light fa-plane-departure',
         subHeader: {
           refresh: true,
         }
@@ -21,10 +21,10 @@ export default {
       path: '/scale/index',
       name: 'qcargoagione.admin.scale',
       crud : import('../_components/scale/index.vue'),
-      page: () => import('@imagina/qcrud/_pages/admin/crudPage'),
-      layout: () => import('@imagina/qsite/_layouts/master.vue'),
+      page: () => import('modules/qcrud/_pages/admin/crudPage'),
+      layout: () => import('layouts/master.vue'),
       title: 'icargo.cms.sidebar.scaleTracking',
-      icon: 'fa-thin fa-weight-scale', 
+      icon: 'fa-thin fa-weight-scale',
       subHeader: {
         refresh: true,
       }
