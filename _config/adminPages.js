8ac2d22538b7f9a864b99607fc3eb5440ab1fbbd
WilmerAce <wilmerc@imaginacolombia.com>
2024-10-23 16:25:13 -0500

    +add: Super administrator permissions are added when published

--- _config/adminPages.js
+++ _config/adminPages.js
@@ -30,7 +30,7 @@ export default {
       }
   },
   schedule: {
-    //permission: 'cargo.scheduled.manage',
+    permission: 'cargo.scheduled.manage',
     activated: true,
     authenticated: true,
     path: '/cargo/schedule/index',
